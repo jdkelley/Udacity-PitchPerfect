addfa07fc13557cd7ade7a48ba7e75e9b4b4f0dd
Joshua Kelley <jdkelley@samaritan.org>
2016-03-24 10:51:55 -0400

    Add Record button. Wire up record button.

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        
+        print("Record Button Was Pressed")
+        recordingLabel.text = "Recording in Progress"
+        
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("stop recording button pressed")
+    }
 }
 
